579d3e0230d9c26da58c1d77dc21aefa00332db6
Mauricio <joanrobayo33@gmail.com>
2025-04-24 22:02:53 -0500

    Modelo de Usuario creado

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('¡Servidor funcionando correctamente!');
+});
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ Conectado a MongoDB'))
+.catch((error) => console.error('❌ Error al conectar a MongoDB:', error));
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor en ejecución en http://localhost:${PORT}`);
+});
 
